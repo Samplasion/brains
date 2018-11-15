d6547e0cddef6908fe03b31294c71943f46d24c7
Samplasion <samplasion@gmail.com>
2018-11-15 18:14:49 +0100

    Looks like this doesn't wanna work in projects

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const BrainMeme = require("./index.js")
 const fs = require('fs');
 
-new BrainMeme("hi", "hello").build().then(buffer => {
+new BrainMeme("hi", "hello", "welcome", "...").build().then(buffer => {
   var wstream = fs.createWriteStream('./img.png');
   wstream.write(buffer)
   wstream.end()
