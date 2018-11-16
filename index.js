7377f32fe7ac84cd0a9c805e9909fcf4ef14dc79
Samplasion <samplasion@gmail.com>
2018-11-16 06:20:05 +0100

    brains-api: the hopefully working update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Jimp = require("jimp")
-const imgs = [null, null, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-imgs.map(async it => {if (it) return await Jimp.read(`./templates/${it}.png`)})
+const imgs = [null, null, "https://cdn.discordapp.com/attachments/485136690906660864/512857019753955328/2.jpg", "https://cdn.discordapp.com/attachments/485136690906660864/512857024191791104/3.jpg", "https://cdn.discordapp.com/attachments/485136690906660864/512857029019172865/4.jpg", "https://cdn.discordapp.com/attachments/485136690906660864/512857032995635202/5.jpg", "https://cdn.discordapp.com/attachments/485136690906660864/512857041317003274/6.jpg", "https://cdn.discordapp.com/attachments/485136690906660864/512857049651216388/7.jpg", "https://cdn.discordapp.com/attachments/485136690906660864/512857058014527498/8.jpg", "https://cdn.discordapp.com/attachments/485136690906660864/512857067850301440/9.jpg", "https://cdn.discordapp.com/attachments/485136690906660864/512857068697550852/10.jpg"]
 
 module.exports = class BrainMeme {
   constructor(...strs) {
@@ -25,7 +24,7 @@ module.exports = class BrainMeme {
   async build() {
     // 176x39 (176x78)
     let y = [24, 147, 272, 391, 544, 703, 868, 1083, 1316, 1505]
-    let image = imgs[this.strings.length]
+    let image = await Jimp.read(imgs[this.strings.length])
     let font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
     this.strings.forEach((s, i) => {
       image.print(font, 0, y[i], {
